083a9943f9a10db9aa9847525d9cfaabaa89978d
Michel <michel.alex.partenaire@gmail.com>
2024-04-06 09:51:48 +0200

    reset pour video

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
-
+import logoKyra from "/images/LogoKyra.png";
 
 const Footer = () => {
   return (
     <footer>
-      <img src="/images/LogoKyra.png" alt="Logo Kyra" />
+      <img src={logoKyra} alt="Logo Kyra" />
       <p>Kyra Coding &copy; 2021</p>
     </footer>
   );
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,11 @@
 import Thumb from "../Thumb";
 import livres from "../../data/livres.json";
-
+import imgBanner from "/images/horizon-HD.webp"
 const Home = () => {
   return (
     <div className="home">
       <div className="banner">
-        <img src="/images/horizon-HD.webp" alt="Lisez, chez vous et ailleurs" />
+        <img src={imgBanner} alt="Lisez, chez vous et ailleurs" />
       <h1>Lisez, chez vous et ailleurs</h1>
       </div>
       <div className="gallery">
